11a11ac87edd02ec685b53fc83190d4d56875b94
Manuel <manuel.vanek@gmail.com>
2023-02-14 14:16:43 +0100

    fixed filter

--- src/App.tsx
+++ src/App.tsx
@@ -29,9 +29,7 @@ function App() {
   function filterNotes(type: string) {
     if (type !== "Both") {
       filtered = true;
-      setFilteredNotesByType(
-        notes.filter((note) => note.type === type)
-      );
+      setFilteredNotesByType(notes.filter((note) => note.type === type));
     } else {
       filtered = false;
       setFilteredNotesByType(notes);
@@ -39,13 +37,10 @@ function App() {
   }
 
   function deleteNote(id: number) {
-    if (filtered) {
-      setFilteredNotesByType(
-        filteredNotesByType.filter((note) => note.id !== id)
-      );
-    } else {
-      setFilteredNotesByType(notes.filter((note) => note.id !== id));
-    }
+    setFilteredNotesByType(
+      filteredNotesByType.filter((note) => note.id !== id)
+    );
+    setNotes(notes.filter((note) => note.id !== id));
   }
 
   function changeType(id: number, type: string) {
@@ -57,16 +52,14 @@ function App() {
       );
     } else {
       setNotes(
-        notes.map((note) =>
-          note.id === id ? { ...note, type: type } : note
-        )
+        notes.map((note) => (note.id === id ? { ...note, type: type } : note))
       );
     }
   }
   return (
     <div>
       <Header newNote={newNote} filterNotes={filterNotes} />
-    
+
       <div>
         {filtered ? (
           <Note
